0e644bb8da1f097935eca17a7d9436973dda07fb
rexy91 <54859814+rexy91@users.noreply.github.com>
2019-12-19 14:38:10 -0500

    Update ticket.rb

--- Mod2/ActiveRecord Associations/app/models/ticket.rb
+++ Mod2/ActiveRecord Associations/app/models/ticket.rb
@@ -1,11 +1,9 @@
-require_relative './passenger.rb'
 class Ticket  
-    belongs_to :passenger 
-    belongs_to :flight
-
+ 
     @@all = [] 
 
     attr_accessor :flight_id, :passenger_id
+    
     def initialize(passenger_id, flight_id)
         @passenger_id = passenger_id
         @flight_id = flight_id
@@ -16,13 +14,15 @@ class Ticket
     def self.all 
         @@all 
     end
-
+    
+    # belongs_to :passenger 
     def passenger
         Passenger.all.select do |passenger|
             passenger.passenger_id == self.passenger_id 
         end 
     end 
-    
+     
+    # belongs_to :flight
     def flight
         Flight.all.select do |flight|
             flight.flight_id == self.flight_id
